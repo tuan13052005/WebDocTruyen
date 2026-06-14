9de82e9ec4ae3b260870f294b6e7918a4b0474cd
tuan13052005 <phamanht7@gmail.com>
2026-06-14 09:48:04 +0700

    update view

--- WebDocTruyen.Web/Controllers/HomeController.cs
+++ WebDocTruyen.Web/Controllers/HomeController.cs
@@ -1,7 +1,9 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using WebDocTruyen.Application.Mapper;
 using WebDocTruyen.Application.Services;
+using WebDocTruyen.Domain.Interfaces;
 using WebDocTruyen.Web.Models;
 
 namespace WebDocTruyen.Web.Controllers
@@ -9,18 +11,21 @@ namespace WebDocTruyen.Web.Controllers
     public class HomeController : Controller
     {
         private readonly StoryService _storyService;
+        private readonly IGenreRepository _genreRepo;
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger, StoryService storyService)
+        public HomeController(ILogger<HomeController> logger, StoryService storyService, IGenreRepository genreRepo)
         {
             _logger = logger;
             _storyService = storyService;
+            _genreRepo = genreRepo;
         }
 
         [AllowAnonymous]
         public async Task<IActionResult> Index()
         {
             var stories = await _storyService.GetAllStoriesAsync();
+            ViewBag.Genres = (await _genreRepo.GetAllGenreAsync()).Select(GenreMapper.ToDto).ToList();
             return View(stories.OrderByDescending(s => s.CreatedAt).Take(8).ToList());
         }
 
